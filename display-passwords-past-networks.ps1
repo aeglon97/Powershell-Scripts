1d00cd195cd76b667133d2fcc8a60ae5bc818408
Marco <aeglon97@gmail.com>
2023-09-09 23:42:55 -0400

    Display list of networks before user input

--- display-passwords-past-networks.ps1
+++ display-passwords-past-networks.ps1
@@ -1,7 +1,5 @@
-Write-Host "Copy and paste the LAN SSID you'd like to retrieve the password of."
+netsh wlan show profile
 
 $name = Read-Host -Prompt "Enter the WLAN SSID whose password you'd like to retrieve."
-
-
 
 netsh wlan show profile $name key=clear
